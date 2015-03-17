286c1dd5b03f0a19eb854d19db36bab34466f5d4
Ryan <ryan@imginconline.com>
2015-03-17 16:55:06 -0400

    Added karma:continuous

--- gruntfile.js
+++ gruntfile.js
@@ -56,7 +56,12 @@ module.exports = function(grunt) {
     karma: {
       unit: {
         configFile: "karma.conf.js"
-      }
+      },
+        continuous: {
+            configFile: 'karma.conf.js',
+            singleRun: true,
+            browsers: ['PhantomJS']
+        }
     }
   });
 
