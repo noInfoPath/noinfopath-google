8956209f8b5d7d4484edc96ed9b39b6af91a9cf4
RyeGuy <ryan@imginconline.com>
2015-05-20 12:31:50 -0400

    Updated gruntfile with copy:lib, bumpup, and version and also cleaned up package.json

--- gruntfile.js
+++ gruntfile.js
@@ -4,46 +4,13 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    bumpup: {
-            file: 'package.json'
-        },
-    gitadd: {
-          task: {
-            options: {
-              force: false
-            },
-            files: {
-              src: ['./']
-            }
-          }
-        },
-    gitcommit: {
-        your_target: {
-          options: {
-            message: ''
-          },
-
-          files: {
-
-          }
-        }
-      },
-    gitpush: {
-        your_target: {
-          options: {
-            remote: '',
-            branch: 'master'
-          }
-        }
-      },
-    markdox : {
-      target : {
-        files: [{
-          src: '/src/*.js',
-          dest: 'README.md'
-          }]
-        }
-      },
+    copy: {
+      build: {
+        files: [
+          {expand:true, flatten:true, src: ['src/noinfopath-google.js'], dest: 'dist/', isFile: true}
+        ]
+      }
+    },
     watch: {
         dev: {
           files: ['src/*.*'],
@@ -62,19 +29,26 @@ module.exports = function(grunt) {
             singleRun: true,
             browsers: ['PhantomJS']
         }
+    },
+    bumpup: {
+            file: 'package.json'
+    },
+    version: {
+      options: {
+        prefix: '@version\\s*'
+      },
+      defaults: {
+        src: ['src/noinfopath-google.js']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-markdox');
   grunt.loadNpmTasks('grunt-bumpup');
-  grunt.loadNpmTasks('grunt-git');
+  grunt.loadNpmTasks('grunt-version');
 
   //Default task(s).
-  grunt.registerTask('documentation', ['markdox']);
-  grunt.registerTask('production', ['copy:production']);
-  grunt.registerTask('development', ['copy:development']);
-  grunt.registerTask('deploy', ['gitadd','gitcommit','gitpush']);
+  grunt.registerTask('build', ['karma:continuous','bumpup','version', 'copy:build']);
 };
